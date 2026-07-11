0e99a6950cb93328d29f92eed21d0334ea69306c
alson-ang <alson.ang@computhink.com.sg>
2026-07-11 06:20:45 +0000

    Jul 11, 2026, 6:20 AM

--- L01_redcircle.js
+++ L01_redcircle.js
@@ -4,7 +4,7 @@ function setup(){
 }
 
 function draw(){
-    fill(255, 0, 0);
+    fill(255, 0, 0);            // Colour inside shape
     ellipse(300,200,200,200);
     
     // Todo: Challenge 3 Traffic Light
