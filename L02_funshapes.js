b9e32deebd6aecbb3109fac2e6416a2b211a6a42
alson-ang <alson.ang@computhink.com.sg>
2026-07-11 07:14:12 +0000

    Jul 11, 2026, 7:14 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -22,7 +22,7 @@ function draw() {
 
   // Challenge 3: A house using rect() function
   rect(50, 50, 500, 300);
-  
+  circle(150, 100, 100);
 
   // Challenge 4: A rocketship using triangle() function
 
