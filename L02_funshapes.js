d3df267a1a03213b3bb2949d8e7ec91842097420
alson-ang <alson.ang@computhink.com.sg>
2026-07-18 06:15:39 +0000

    Jul 18, 2026, 6:15 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -32,21 +32,21 @@ function draw() {
   
   // Challenge 5: A square face using quad() function
   quad(100, 50, 500, 50, 500, 350, 100, 350); // (x1, y1, x2, y2, x3, y3, x4, y4)
-    ellipse(300, 200, 400, 400); //
+  ellipse(300, 200, 400, 400);
   
-    fill(0);
-    ellipse(240, 170, 50, 70); // Left eye
-    ellipse(360, 200, 50, 50); // Right eye
-  
-    fill(255, 0, 0); // Red
-    ellipse(300, 280, 150, 60); // Mouth
-  
-    fill(255); // White
-    ellipse(300, 270, 140, 30); // Teeth
-  
-    fill(255, 204, 0); // Yellow
-    ellipse(100, 60, 90, 80); // Ear left
-    ellipse(500, 60, 90, 80); // Funny ear
+  fill(0);
+  ellipse(240, 170, 50, 70);
+  ellipse(360, 200, 50, 50); // Right eye
+
+  fill(255, 0, 0); // Red
+  ellipse(300, 280, 150, 60); // Mouth
+
+  fill(255); // White
+  ellipse(300, 270, 140, 30); // Teeth
+
+  fill(255, 204, 0); // Yellow
+  ellipse(100, 60, 90, 80); // Ear left
+  ellipse(500, 60, 90, 80); // Funny ear
   // Challenge 6: Add some colours to the drawings in the above challenges
 
   // Challenge 7: Create a funny Pokemon
