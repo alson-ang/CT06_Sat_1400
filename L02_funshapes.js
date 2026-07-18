f1309490e3358a018861183625d658ab9aa2d1ce
alson-ang <alson.ang@computhink.com.sg>
2026-07-18 06:25:26 +0000

    Jul 18, 2026, 6:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -33,11 +33,14 @@ function draw() {
   // Challenge 5: A square face using quad() function
   fill(66, 179, 245); // (r, g, b)
   rect(100, 50, 400, 300);
-  
+
   fill(150); // (grayscale 0-255)
   quad(200, 250, 400, 250, 350, 300, 250, 300); // (x1, y1, x2, y2, x3, y3, x4, y4)
+
   fill("#42b3f5");  // ("#hex")
   ellipse(240, 170, 50, 70);
+
+  
   ellipse(360, 200, 50, 50);
   ellipse(100, 60, 90, 80);
   ellipse(500, 60, 90, 80);
