d73602ecb1bcd7d52ffc7445c8bc6b1ea8493929
alson-ang <alson.ang@computhink.com.sg>
2026-07-18 07:15:38 +0000

    Jul 18, 2026, 7:15 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -30,8 +30,8 @@ function draw() {
   //   console.log(i);
   // }
   // Task 1: Colour Gradient
-  for (let i = 0; i < 10; i++) {
-    console.log(i);
+  for (let i = 1; i < 10; i++) {
+    
   }
 
   // Task 2: Colour Loop
