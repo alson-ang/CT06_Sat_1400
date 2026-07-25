66080c201b403acf1c5a254f63dedfcaaf743c18
alson-ang <alson.ang@computhink.com.sg>
2026-07-25 07:19:03 +0000

    Jul 25, 2026, 7:19 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -75,6 +75,7 @@ function draw() {
   let circleDiameter = 30;
   let numCircles = 5;
   let totalWidth = numCircles * circleDiameter;
+  
   // Calculate starting x for centering​
   let startX = (width - totalWidth) / 2 + circleDiameter / 2;
   for (let i = 0; i < numCircles; i++) {
