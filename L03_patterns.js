868296b6d8e288751a17030b56fcf22621c436b5
alson-ang <alson.ang@computhink.com.sg>
2026-07-25 06:22:21 +0000

    Jul 25, 2026, 6:22 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -44,6 +44,7 @@ function draw() {
   // Task 2: Colour Loop
   circle(100, 100, 100);  // (x, y, diameter)
   fill(colourValue);  // (grayscale) 0 - 255
+  colourValue++;
 
   // Task 3: Row of Circles
 
