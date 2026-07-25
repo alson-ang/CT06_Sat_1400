58427681b6165880d2d4e160afbabcdace761627
alson-ang <alson.ang@computhink.com.sg>
2026-07-25 06:54:21 +0000

    Jul 25, 2026, 6:54 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -60,7 +60,12 @@ function draw() {
   // Task 4: Grid of Circles
   for (row = 0; row < 5; row++) {
     for (col = 0; col < 5; col++) {
-      fill(0);
+      // if () {
+      //   fill(0);
+      // } else {
+      //   fill(255);
+      // }
+      
       rect(col * 50, row * 50, 50, 50);
     }
   }
