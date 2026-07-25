43173a8bc8e96e8a396f974fa130e0d7f8843d93
alson-ang <alson.ang@computhink.com.sg>
2026-07-25 07:33:19 +0000

    Jul 25, 2026, 7:33 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -81,6 +81,7 @@ function draw() {
   let startY = (height - totalWidth) / 2 + circleDiameter / 2;
 
   for (row = 0; row < 5; row++) {
+    
     for (let i = 0; i < numCircles; i++) {
       let x = startX + i * circleDiameter;
       ellipse(x, height / 2, circleDiameter, circleDiameter);
