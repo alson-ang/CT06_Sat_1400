6091faab83ad8585d6b3724a62708ffca05adfc9
alson-ang <alson.ang@computhink.com.sg>
2026-08-22 06:12:12 +0000

    Aug 22, 2026, 6:12 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -37,9 +37,9 @@ function draw() {
     y = constrain(y, 0, height);
 
     if (direction == "up") {
-        triangle(75, y + 25, 100, y, 125, y + 25);
+        triangle(75, y + 50, 100, y, 125, y + 50);
     } else if (direction == "down") {
-        triangle(75, y - 25, 100, y, 125, y - 25);
+        triangle(75, y - 50, 100, y, 125, y - 50);
     }
 }
 
