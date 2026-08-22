18f6d2166a4518ab5dc1791bd15d7acf2d0dcd10
alson-ang <alson.ang@computhink.com.sg>
2026-08-22 07:13:07 +0000

    Aug 22, 2026, 7:13 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -35,7 +35,7 @@ function draw() {
     y = constrain(x, 0, width - 100);
     
     // Draw the static image
-    image(staticImage, x, y, 100, 100); // Display static image
+    image(staticImage, x, y, 100, 100); // (image, x pos, y pos, )
 }
 
 function keyPressed() {
