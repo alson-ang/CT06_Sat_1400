8534bcaa60fe5ddc3088303ea93a3516af702299
alson-ang <alson.ang@computhink.com.sg>
2026-08-22 07:30:04 +0000

    Aug 22, 2026, 7:30 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -32,7 +32,8 @@ function draw() {
         x += speed;
     }
     
-    // Check if x / y is at 
+    // Check if x / y is at min or max
+    if (x === 0 || x === width - 100)
 
     // Constrain static image within canvas boundaries
     x = constrain(x, 0, width - 100);
