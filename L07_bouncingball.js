d510b8ce3e82792f5b3529c7ff95eb79fb537c2a
alson-ang <alson.ang@computhink.com.sg>
2026-08-29 06:38:29 +0000

    Aug 29, 2026, 6:38 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -35,6 +35,8 @@ function draw() {
     x = constrain(x, 0, width - 100);   // (variable, min, max)
     y = constrain(y, 0, height - 100);
 
+    
+
     // Draw pico
     image(picoImage, x, y, 100, 100);  // (image, x pos, y pos, width, height)
 }
