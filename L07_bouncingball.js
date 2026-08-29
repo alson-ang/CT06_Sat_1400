2f7064aa09a1f9d80660172e3725b231e643c113
alson-ang <alson.ang@computhink.com.sg>
2026-08-29 06:10:38 +0000

    Aug 29, 2026, 6:10 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -3,6 +3,6 @@ let picoImage;
 let popSound;
 
 function preload() {
-    picoImage = loadImage("assets/pico-a.png")
-    popSound = loadSound("assets/pop.mp3")
+    picoImage = loadImage("assets/pico-a.png");
+    popSound = loadSound("assets/pop.mp3");
 }
