e57e1d9c4f73669f31ad1403dc4adf4d00d37b6f
alson-ang <alson.ang@computhink.com.sg>
2026-09-12 06:37:03 +0000

    Sep 12, 2026, 6:37 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -14,6 +14,7 @@ function draw() {
 
     // Movement
     // if square's x pos goes past right or left side
+    // width - square size
     if (squareX > width - 100 || squareX < 0) {
         // invert the speed to change directions
         squareSpeed *= -1;
