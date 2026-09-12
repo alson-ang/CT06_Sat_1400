c609ffcec7816e6a412a710146656d1f5cee976c
alson-ang <alson.ang@computhink.com.sg>
2026-09-12 06:44:23 +0000

    Sep 12, 2026, 6:44 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -34,7 +34,7 @@ function setup() {
     // Set text size
     textSize(40);
     // Write text on canvas
-    text("asdasd", width / 2, height / 2); // (str)
+    text("asdasd", width / 2, height / 2); // (string, x pos, y pos)
 }
 
 function draw() {
