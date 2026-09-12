c4a1c1ef6a038854dad095f614f7fea3f5b9c3ac
alson-ang <alson.ang@computhink.com.sg>
2026-09-12 06:41:46 +0000

    Sep 12, 2026, 6:41 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,32 +1,32 @@
-// // Recap
-// let squareX = 300;
-// let squareY = 200;
-// let squareSpeed = 5;
-
-// function setup() {
-//     createCanvas(600, 400);
-//     background(220);
-// }
-
-// function draw() {
-//     // Reset canvas / clear screen
-//     background(220);
-
-//     // Movement
-//     // if square's x pos goes past right or left side
-//     // width - square size
-//     if (squareX > width - 100 || squareX < 0) {
-//         // invert the speed to change directions
-//         // positive to negative and negative to positive
-//         squareSpeed *= -1;
-//     }
-
-//     squareX += squareSpeed;
-
-//     fill(255, 255, 0);
-//     rect(squareX, squareY, 100, 100); // (x pos, y pos, width, height)
-// }
+// Recap
+let squareX = 300;
+let squareY = 200;
+let squareSpeed = 5;
 
 function setup() {
-    
+    createCanvas(600, 400);
+    background(220);
+}
+
+function draw() {
+    // Reset canvas / clear screen
+    background(220);
+
+    // Movement
+    // if square's x pos goes past right or left side
+    // width - square size
+    if (squareX > width - 100 || squareX < 0) {
+        // invert the speed to change directions
+        // positive to negative and negative to positive
+        squareSpeed *= -1;
+    }
+
+    squareX += squareSpeed;
+
+    fill(255, 255, 0);
+    rect(squareX, squareY, 100, 100); // (x pos, y pos, width, height)
+}
+
+function setup() {
+
 }
