e1bb030a38e1ad322f92a16d8fe11843ec4d6217
alson-ang <alson.ang@computhink.com.sg>
2026-09-12 06:33:06 +0000

    Sep 12, 2026, 6:33 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -10,12 +10,12 @@ function setup() {
 
 function draw() {
     // Movement
-    squareX += squareSpeed;
-
     // if square's x pos goes past right side
     if (squareX > width) {
         squareSpeed *= -1;
     }
+    
+    squareX += squareSpeed;
 
     fill(255, 255, 0);
     rect(squareX, squareY, 100, 100); // (x pos, y pos, width, height)
