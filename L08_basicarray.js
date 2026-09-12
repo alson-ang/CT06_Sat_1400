743f5597b31a0205fcc929a82ad791b913e7f193
alson-ang <alson.ang@computhink.com.sg>
2026-09-12 07:25:36 +0000

    Sep 12, 2026, 7:25 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -45,6 +45,6 @@ function setup() {
 function draw() {
 
     for (let i = 0; i < favouriteFoods.length; i++) {
-        text((i + 1) + ". " + favouriteFoods[i], width / 2, y)
+        text((i + 1) + ". " + favouriteFoods[i], width / 2, yPos + i * 50);
     }    
 }
