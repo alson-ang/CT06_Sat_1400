07a06315fb9b5cd204a9fd550e13441d952e897b
alson-ang <alson.ang@computhink.com.sg>
2026-09-12 06:21:27 +0000

    Sep 12, 2026, 6:21 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,11 +1,13 @@
 // Recap
+let x
+
 function setup() {
     new Canvas(600, 400);
     background(220);
 }
 
 function draw() {
-    
+
 
     rect(width / 2, height / 2, 100, 100); // (x, y, width, height)
 }
